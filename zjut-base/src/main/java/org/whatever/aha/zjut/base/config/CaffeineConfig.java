813908448dea25130d6f202cf7359f738fd9b50d
Rockobass <l>
2021-07-28 11:20:01 +0800

    添加spring端点配置

--- zjut-base/src/main/java/org/whatever/aha/zjut/base/config/CaffeineConfig.java
+++ zjut-base/src/main/java/org/whatever/aha/zjut/base/config/CaffeineConfig.java
@@ -1,5 +1,6 @@
 package org.whatever.aha.zjut.base.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
---
+++ zjut-base/src/main/java/org/whatever/aha/zjut/base/endpoint/CaffeineEndPoint.java
@@ -0,0 +1,27 @@
+package org.whatever.aha.zjut.base.endpoint;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Endpoint(id = "caffeine")
+public class CaffeineEndPoint {
+    @Autowired
+    CacheManager caffeineCacheManager;
+
+    @ReadOperation
+    public Object getStats() {
+        List<Object> stats = new ArrayList<>();
+        caffeineCacheManager.getCacheNames().forEach(e -> stats.add(((CaffeineCache)caffeineCacheManager.getCache(e)).getNativeCache().stats()));
+        return stats;
+    }
+
+}
